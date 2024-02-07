fa74346a1af390ba11008696b4f7ade02119e677
Jesus <jesus.garcia.m1488@gmail.com>
2024-02-07 14:25:11 -0600

    pagina de mantenimiento agregada no terminada

---
+++ frontend/js/temario_mtto/funciones_temario_mtto.js
@@ -0,0 +1,19 @@
+function mostrar_temario(letra){
+    switch(letra){
+        case 'a':
+            
+            break;
+        case 'b':
+            
+            break;
+        case 'c':
+         
+            break;
+        case 'd':
+           
+            break;
+        case 'e':
+
+            break;
+    }
+}
